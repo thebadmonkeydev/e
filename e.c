d61fbe065cecb582c83f9f8aef56dbae23d7c881
Michael Kelly <michaelkelly322@gmail.com>
2020-05-21 00:00:55 -0400

    Add in depth raw mode

--- e.c
+++ e.c
@@ -16,12 +16,37 @@ disable_raw_mode() {
 
 void
 enable_raw_mode() {
+  // Store original terminal config
   tcgetattr(STDIN_FILENO, &orig_termios);
+
+  // Schedule resetting to original config at exit
   atexit(disable_raw_mode);
 
+  /*
+   * Disable terminal flags:
+   *
+   * ECHO   - By default terminals print the ascii character for key presses automatically
+   * ICANON - Canonical mode, waits to return read() until the user presses ENTER
+   * ISIG   - Respond to ctrl+c and ctrl+z signals
+   * IXON   - Software flow control, ctrl+s and ctrl+q
+   * IEXTEN - ctrl+v character literal input
+   * ICRNL  - Translates carriage returns into line feeds when input by user (fixes ctrl+m)
+   * OPOST  - Translates carriage returns to line feeds during output processing
+   *
+   * Less applicable to modern terminals but are technically part of "raw mode":
+   *
+   * BRKINT - Break conditions sent SIGINT
+   * INPCK  - Parity checking...in a terminal...
+   * ISTRIP - Strips 8th bit of input (probably already turned off)
+   * CS8    - Sets character width to 8-bits (likely already on as well)
+   */
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
+  // Set our custom terminal config
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -35,9 +60,9 @@ main(int argc, char *argv[])
   char c;
   while (read(STDIN_FILENO, &c, 1) && c != 'q') {
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
   }
 
