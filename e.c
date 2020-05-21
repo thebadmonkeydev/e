cebc2ca6eb30b1a8b46932cdc282309fed0ac886
Michael Kelly <michaelkelly322@gmail.com>
2020-05-20 23:38:57 -0400

    Setup raw mode for terminal

--- e.c
+++ e.c
@@ -2,10 +2,36 @@
  *  E
  */
 #include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
 
-int main(int argc, char *argv[])
+struct termios orig_termios;
+
+void
+disable_raw_mode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH,  &orig_termios);
+}
+
+void
+enable_raw_mode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disable_raw_mode);
+
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int
+main(int argc, char *argv[])
 {
+  // Enable raw mode and other convinient terminal settings
+  // runs disable_raw_mode() using atexit()
+  enable_raw_mode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) && c != 'q');
+
   return 0;
 }
