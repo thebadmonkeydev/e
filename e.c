18171020696b52e3454d9f0c9bc576ac54013c95
Michael Kelly <michaelkelly322@gmail.com>
2020-05-27 22:08:30 -0400

    Adds window geometry query

--- e.c
+++ e.c
@@ -5,23 +5,63 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-struct termios orig_termios;
+/* Global editor state */
+struct estate {
+  int rows;
+  int cols;
+  struct termios orig_termios;
+};
+
+struct estate E;
+
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abappend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abfree(struct abuf *ab) {
+  free(ab->b);
+}
 
 void refresh();
 
 void die(const char *s) {
-  refresh();
   perror(s);
   exit(1);
 }
 
+int get_window_size(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 void teardown() {
-  if(tcsetattr(STDIN_FILENO, TCSAFLUSH,  &orig_termios) == -1)
+  if(tcsetattr(STDIN_FILENO, TCSAFLUSH,  &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
@@ -29,7 +69,8 @@ void teardown() {
 // runs teardown() using atexit()
 void setup() {
   // Store original terminal config
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
+  write(STDOUT_FILENO, "\x1b[2J", 4);
 
   // Schedule resetting to original config at exit
   atexit(teardown);
@@ -52,7 +93,7 @@ void setup() {
    * ISTRIP - Strips 8th bit of input (probably already turned off)
    * CS8    - Sets character width to 8-bits (likely already on as well)
    */
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_cflag |= (CS8);
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
@@ -60,6 +101,8 @@ void setup() {
 
   // Set our custom terminal config
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcgetattr");
+
+  if (get_window_size(&E.rows, &E.cols) == -1) die("get_window_size");
 }
 
 
@@ -79,19 +122,30 @@ void process_keys() {
   }
 }
 
-void draw_rows() {
-  for (int y = 0; y < 24; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+void draw_rows(struct abuf *ab) {
+  for (int y = 0; y < E.rows; y++) {
+    abappend(ab, "~", 1);
+
+    abappend(ab, "\x1b[k", 3);
+    if(y < (E.rows - 1)) {
+      abappend(ab, "\r\n", 2);
+    }
   }
 }
 
 void refresh() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  draw_rows();
+  abappend(&ab, "\x1b[?25l", 6);
+  abappend(&ab, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  draw_rows(&ab);
+
+  abappend(&ab, "\x1b[H", 3);
+  abappend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abfree(&ab);
 }
 
 int main(int argc, char *argv[])
